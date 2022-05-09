0b0f321dd9921a67b147f2a0e3cf9780cb0ad163
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-05-09 16:24:51 +0000

    Correct linters errors

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-//const { func } = require("assert-plus");
-
+// const { func } = require("assert-plus");
 const mainHeader = document.querySelector('.mobile-menu');
 
 const menuButton = document.getElementById('menu');
-menuButton.addEventListener('click', function () {
+menuButton.addEventListener('click', () => {
   mainHeader.classList.toggle('mobile-menu-open');
 });
 
 const menuElemLink = document.querySelectorAll('.desktop li a');
 
-
 menuElemLink.forEach((link) => {
-  link.addEventListener('click', function () {
+  link.addEventListener('click', () => {
     mainHeader.classList.remove('mobile-menu-open');
   });
 });
 
-document.getElementById("menu-close").addEventListener('click', function (){
+document.getElementById('menu-close').addEventListener('click', () => {
   mainHeader.classList.remove('mobile-menu-open');
-})
+});
