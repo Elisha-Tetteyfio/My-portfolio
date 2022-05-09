468c4668ee489e95cf23f3f8be050b084ffb38d7
whid <kontakt@cgnk.pl>
2022-05-09 17:04:17 +0200

    Add close mobile menu on click

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const mainHeader = document.querySelector(".mobile-menu")
+const mainHeader = document.querySelector('.mobile-menu');
 
-const menuButton = document.getElementById("menu")
-menuButton.addEventListener("click", function () {
-  mainHeader.classList.toggle("mobile-menu-open")
-})
+const menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', function () {
+  mainHeader.classList.toggle('mobile-menu-open');
+});
+
+const menuElemLink = document.querySelectorAll('.desktop li a');
+
+menuElemLink.forEach((link) => {
+  link.addEventListener('click', function () {
+    mainHeader.classList.remove('mobile-menu-open');
+  });
+});
