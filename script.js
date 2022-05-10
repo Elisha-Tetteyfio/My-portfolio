2ac180ca344793c2c9e4d1c33b028fd9e4ab60d0
whid-bb <whid11s50@gmail.com>
2022-05-10 11:25:45 +0200

    Create my recent works dynamically

--- script.js
+++ script.js
@@ -20,42 +20,86 @@ document.getElementById('menu-close').addEventListener('click', () => {
 
 //////////////////////////////////////////////
 let projects = [
-  project1 = {
-    title : "Multi - Post stories",
-    image : "",
-    description: "",
-    liveDemo: "",
-    source: ""
-  },
-  project2 = {
-    title : "Multi - Post stories",
-    image : "",
-    description: "",
-    liveDemo: "",
-    source: ""
-  },
-  project3 = {
-    title : "Multi - Post stories",
-    image : "",
-    description: "",
-    liveDemo: "",
-    source: ""
-  },
-  project4 = {
-    title : "Multi - Post stories",
-    image : "",
-    description: "",
-    liveDemo: "",
-    source: ""
-  },
+  (project1 = {
+    title: 'Multi - Post stories',
+    image: '',
+    imgAlt: '',
+    description: '',
+    liveDemo: '',
+    source: '',
+    langs:
+      '<li class="project-languages-1">html</li><li class="project-languages-1">css</li>',
+  }),
+  (project2 = {
+    title: 'Multi - Post stories',
+    image: '',
+    imgAlt: '',
+    description: '',
+    liveDemo: '',
+    source: '',
+    langs:
+      '<li class="project-languages-1">html</li><li class="project-languages-1">css</li>',
+  }),
+  (project3 = {
+    title: 'Multi - Post stories',
+    image: '',
+    imgAlt: '',
+    description: '',
+    liveDemo: '',
+    source: '',
+    langs:
+      '<li class="project-languages-1">html</li><li class="project-languages-1">css</li>',
+  }),
+  (project4 = {
+    title: 'Multi - Post stories',
+    image: '',
+    imgAlt: '',
+    description: '',
+    liveDemo: '',
+    source: '',
+    langs:
+      '<li class="project-languages-1">html</li><li class="project-languages-1">css</li>',
+  }),
 ];
 
+const recentWorkContainer = document.getElementById('recent-works');
+const recentWorkHeader = document.createElement('h2');
+const rwGridContainer = document.createElement('div');
+rwGridContainer.classList.add('recent-works-grid');
 
+let projectHTMLcontent = '';
 
-const moreButtons = document.querySelectorAll('n1');
+projects.forEach((el, index) => {
+  projectHTMLcontent += `
+  <div class="works" id="work-1">
+  <div class="desktop-layout ${index % 2 === 0 ? 'second-works' : ''} ">
+      <img src="${el.image}" alt="${el.imgAlt}">
+      <div class="desktop-layout-1">
+          <h4>${el.title}</h4>
+          <p>${el.description}
+          </p>
+          <ul class="project-languages">
+              ${el.langs}
+          </ul>
+          <div class="n1"><button data-project="${index}">See Project</button></div>
+      </div>
+  </div>
+</div>
+  `;
+});
 
-moreButtons.forEach(element => {
+rwGridContainer.innerHTML = projectHTMLcontent;
+
+recentWorkHeader.innerText = 'MY RECENT WORKS';
+
+recentWorkContainer.prepend(rwGridContainer);
+recentWorkContainer.prepend(recentWorkHeader);
+
+const moreButtons = document.querySelectorAll('.n1 button');
+
+moreButtons.forEach((element) => {
   element.addEventListener('click', () => {
-    mainHeader.classList.toggle('details-open');
-  }); 
+    console.log(element.dataset.project);
+    //   mainHeader.classList.toggle('details-open');
+  });
 });
