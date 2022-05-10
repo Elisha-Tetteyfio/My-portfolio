ddbd7d6ac9ac862469e8b5048ce61f6c5b05e3d8
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-05-10 09:00:37 +0000

    Add project object

--- script.js
+++ script.js
@@ -17,3 +17,45 @@ menuElemLink.forEach((link) => {
 document.getElementById('menu-close').addEventListener('click', () => {
   mainHeader.classList.remove('mobile-menu-open');
 });
+
+//////////////////////////////////////////////
+let projects = [
+  project1 = {
+    title : "Multi - Post stories",
+    image : "",
+    description: "",
+    liveDemo: "",
+    source: ""
+  },
+  project2 = {
+    title : "Multi - Post stories",
+    image : "",
+    description: "",
+    liveDemo: "",
+    source: ""
+  },
+  project3 = {
+    title : "Multi - Post stories",
+    image : "",
+    description: "",
+    liveDemo: "",
+    source: ""
+  },
+  project4 = {
+    title : "Multi - Post stories",
+    image : "",
+    description: "",
+    liveDemo: "",
+    source: ""
+  },
+];
+
+
+
+const moreButtons = document.querySelectorAll('n1');
+
+moreButtons.forEach(element => {
+  element.addEventListener('click', () => {
+    mainHeader.classList.toggle('details-open');
+  }); 
+});
